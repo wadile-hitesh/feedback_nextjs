1dddca94ef4316a2c8dcbaa2011ed1f729d3a36e
wadile-hitesh <hiteshwadile.dev@gmail.com>
2024-04-17 18:11:17 +0530

    Next auth setup

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -9,8 +9,84 @@ export async function POST(request : Request, ){
 
     try {
         const {username ,email, password} = await request.json()
+        
+        const existingUserIsVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified : true
+        })
 
-    
+        if(existingUserIsVerifiedByUsername){
+            return Response.json({
+                success : false,
+                message : "Username already exists"
+            }, {status : 400})
+        }
+
+        const existingUserByEmail =await UserModel.findOne({
+            email
+        })
+        let verifyCode = Math.floor(
+          100000 + Math.random() * 900000
+        ).toString();
+
+        if(existingUserByEmail){
+            if(existingUserByEmail.isVerified){
+                return Response.json({
+                    success : false,
+                    message : "User with this Email already exists"
+                })
+            }
+            else{
+                const hashedPassword = await bcrypt.hash(password, 10)
+
+                existingUserByEmail.password = hashedPassword
+                existingUserByEmail.verifyCode = verifyCode
+                existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+
+                await existingUserByEmail.save()
+            }
+        }
+        else{
+            const hashedPassword = await bcrypt.hash(password, 10)
+            
+            const expiryDate = new Date()
+            expiryDate.setHours(expiryDate.getHours() + 1)
+
+            const newUser = await new UserModel({
+                username,
+                email,
+                password: hashedPassword,
+                verifyCode,
+                verifyCodeExpiry: expiryDate,
+                isVerified: false,
+                isAcceptingMessage: true,
+                message: []
+            })
+
+            await newUser.save()
+        }
+        // send Verification Email
+
+        const emailResponse = await sendVerificationEmail(
+            email,
+            username,
+            verifyCode
+        );
+
+        if(!emailResponse.success){
+            return Response.json({
+                success : false,
+                message : emailResponse.message
+            }, {status : 500})
+        }
+        
+        return Response.json(
+            {
+            success: true,
+            message: "User Registered Successfully and Verification Email Sent",
+            },
+            { status: 201 }
+        );
     } catch (error) {
         console.error('Error while Registering User', error)
         return Response.json({
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,30 @@
+import 'next-auth'
+import { DefaultSession } from 'next-auth'
+
+declare module 'next-auth' {
+    interface User {
+        _id? : string,
+        isVerified? : boolean
+        isAcceptinMessages? : boolean,
+        username? : string,
+    }
+    interface Session  {
+        user : {
+            _id? : string,
+            isVerified? : boolean
+            isAcceptinMessages? : boolean,
+            username? : string,
+        } & DefaultSession['user']
+    }
+}
+
+// another way to declare the types
+
+declare module 'next-auth/jwt'{
+    interface JWT {
+        _id? : string,
+        isVerified? : boolean
+        isAcceptinMessages? : boolean,
+        username? : string,
+    }
+}
